2c521063c18eb1d2762332a8d003bd28f532d8bf
AleksandraPestka <aleksandrapestka@wp.pl>
2021-11-15 13:48:13 +0100

    Streamline length computation repaired

--- src/generate_connectivity_matrix.py
+++ src/generate_connectivity_matrix.py
@@ -2,11 +2,13 @@
 
 import os
 import logging
+from nibabel import streamlines
 
 import numpy as np
 from nibabel.streamlines import load
 import nibabel
 from dipy.tracking import utils 
+from dipy.tracking.streamline import length
 import matplotlib.pyplot as plt
 import yaml
 
@@ -18,9 +20,23 @@ def get_paths(config):
                                    f"tractogram_{config['paths']['subject']}_ses-1_acq-AP_dwi_ACT.trk")
     return output_dir, atlas_path, tractogram_path
 
-def remove_short_connections(streamlines, thres=30):
-    longer_streamlines = [t for t in streamlines if len(t)>thres]
+def remove_short_connections(streamlines, len_thres=30):
+    ''' Filter streamlines with the length shorter 
+    than provided threshold [usually in mm]. '''
+    longer_streamlines = [s for s in streamlines
+                          if compute_streamline_length(s)>len_thres]
     return longer_streamlines
+
+def compute_streamline_length(streamline, is_dipy=True):
+    if is_dipy:
+        # use built-in DIPY function to calculate the length of streamline
+        return length(streamline)
+    else:
+        # use classical method with calculating vector norm
+        streamline = np.array(streamline)
+        s_length = np.sum([np.linalg.norm(streamline[i+1]-streamline[i]) 
+                    for i in range(0, len(streamline)-1)])
+    return s_length
 
 def load_atlas(path):
     atlas = nibabel.load(path)
@@ -70,15 +86,18 @@ def main():
     output_dir, atlas_path, tractogram_path =  get_paths(config)
 
     tractogram = load(tractogram_path)
+    streamlines = tractogram.streamlines
     logging.info(f"Loading tractogram from subject: {config['paths']['subject']}")
-    logging.info(f'No. of streamlines: {np.shape(tractogram.streamlines)}')
+    logging.info(f'No. of streamlines: {np.shape(streamlines)}')
+   
+    streamlines = remove_short_connections(streamlines)
 
     affine = tractogram.affine # transformation to align streamlines to atlas 
     atlas, labels  = load_atlas(atlas_path)
     logging.info(f'No. of unique atlas labels: {len(np.unique(labels))}, \
         min value: {np.min(labels)}, max value: {np.max(labels)}')
 
-    connect_matrix = create_connectivity_matrix(tractogram.streamlines, 
+    connect_matrix = create_connectivity_matrix(streamlines, 
                                                 affine, 
                                                 labels,
                                                 config['tractogram_config']['take_log'])
