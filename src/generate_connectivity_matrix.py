700b42809d6e6f2ccae9562b2a90dddb3dedce18
AleksandraPestka <aleksandrapestka@wp.pl>
2021-11-16 10:46:02 +0100

    connectivity matrix class created

--- src/generate_connectivity_matrix.py
+++ src/generate_connectivity_matrix.py
@@ -8,6 +8,7 @@ from nibabel.streamlines import load
 import nibabel
 from dipy.tracking import utils 
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import take
 import yaml
 
 def get_paths(config):
@@ -23,39 +24,61 @@ def load_atlas(path):
     labels = atlas.get_fdata().astype(np.uint8)
     return atlas, labels   
 
-def create_connectivity_matrix(streamlines, affine, labels, reshuffle=True, take_log=True):
-    ''' Get the no. of connections between each pair of brain regions. '''
-    M, _ = utils.connectivity_matrix(streamlines, 
-                                    affine=affine, 
-                                    label_volume=labels, 
-                                    return_mapping=True,
-                                    mapping_as_streamlines=True)
-    # remove background
-    M = M[1:, 1:]
+class ConnectivityMatrix():
+    def __init__(self, tractogram, atlas_labels, output_dir, take_log):
+        self.streamlines = tractogram.streamlines
+        self.affine = tractogram.affine # transformation to align streamlines to atlas 
+        self.labels = atlas_labels  
+        self.output_dir = output_dir
+        self.take_log = take_log
+                       
+    def __create(self, reshuffle=True):
+        ''' Get the no. of connections between each pair of brain regions. '''
+        M, _ = utils.connectivity_matrix(self.streamlines, 
+                                        affine=self.affine, 
+                                        label_volume=self.labels, 
+                                        return_mapping=True,
+                                        mapping_as_streamlines=True)
+        # remove background
+        M = M[1:, 1:]
 
-    if reshuffle:
-        # make all left areas first 
-        odd_odd = M[::2, ::2]
-        odd_even = M[::2, 1::2]
-        first = np.vstack((odd_odd, odd_even))
-        even_odd = M[1::2, ::2]
-        even_even= M[1::2, 1::2]
-        second = np.vstack((even_odd, even_even))
-        M = np.hstack((first,second))
+        if reshuffle:
+            # make all left areas first 
+            odd_odd = M[::2, ::2]
+            odd_even = M[::2, 1::2]
+            first = np.vstack((odd_odd, odd_even))
+            even_odd = M[1::2, ::2]
+            even_even= M[1::2, 1::2]
+            second = np.vstack((even_odd, even_even))
+            M = np.hstack((first,second))
 
-    # remove connections to own regions (inplace)
-    np.fill_diagonal(M, 0) 
-    if take_log: M = np.log1p(M)
+        # remove connections to own regions (inplace)
+        np.fill_diagonal(M, 0) 
+        if self.take_log: M = np.log1p(M)
 
-    return M
+        self.matrix = M
+        
+    def __save(self):
+        np.savetxt(os.path.join(self.output_dir, 'connect_matrix.csv'), 
+                   self.matrix, delimiter=',')
 
-def plot_connectivity_matrix(matrix, output_dir, take_log=True):
-    plt.figure(figsize=(8, 6))
-    plt.imshow(matrix, interpolation='nearest')
-    plt.colorbar()
-    plt.title(f'Connectivity matrix (log values: {take_log})')
-    plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, 'connect_matrix.png'))
+    def __plot(self, savefig=True):
+        plt.figure(figsize=(8, 6))
+        plt.imshow(self.matrix, interpolation='nearest')
+        plt.colorbar()
+        plt.title(f'Connectivity matrix (log values: {self.take_log})')
+        plt.tight_layout()
+        if savefig: plt.savefig(os.path.join(self.output_dir, 'connect_matrix.png'))
+        
+    def __get_info(self):
+        logging.info(f'Shape of connectivity matrix: {self.matrix.shape}. \
+        Sum of values: {np.sum(self.matrix)} (after removing background and connections to own regions)')
+        
+    def process(self):
+        self.__create()   
+        self.__get_info()
+        self.__save()
+        self.__plot()     
 
 def main():
     logging.basicConfig(level=logging.INFO)
@@ -64,30 +87,16 @@ def main():
         config = yaml.load(f, Loader=yaml.FullLoader)
         
     output_dir, atlas_path, tractogram_path =  get_paths(config)
+    logging.info(f"Loading tractogram from subject: {config['paths']['subject']}")
 
     tractogram = load(tractogram_path)
-    streamlines = tractogram.streamlines
-    logging.info(f"Loading tractogram from subject: {config['paths']['subject']}")
-    logging.info(f'No. of streamlines: {np.shape(streamlines)}')
-   
-    affine = tractogram.affine # transformation to align streamlines to atlas 
     atlas, labels  = load_atlas(atlas_path)
     logging.info(f'No. of unique atlas labels: {len(np.unique(labels))}, \
         min value: {np.min(labels)}, max value: {np.max(labels)}')
-
-    connect_matrix = create_connectivity_matrix(streamlines, 
-                                                affine, 
-                                                labels,
-                                                config['tractogram_config']['take_log'])
     
-    np.savetxt(os.path.join(output_dir, 'connect_matrix.csv'), 
-               connect_matrix, delimiter=',')
-    logging.info(f'Shape of connectivity matrix: {connect_matrix.shape}. \
-        Sum of values: {np.sum(connect_matrix)} (after removing background and connections to own regions)')
-
-    plot_connectivity_matrix(connect_matrix, output_dir, 
-                             config['tractogram_config']['take_log'])
-
+    cm = ConnectivityMatrix(tractogram, labels, output_dir, 
+                            config['tractogram_config']['take_log'])
+    cm.process()
 
 if __name__ == '__main__':
     main()
