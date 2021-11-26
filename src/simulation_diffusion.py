7d3eb6d00bc16f13ef0876e10c2baa1d7c0bac5c
AleksandraPestka <aleksandra.anna.pestka@gmail.com>
2021-11-26 09:53:35 +0100

    alternative interation step func addded

--- src/simulation_diffusion.py
+++ src/simulation_diffusion.py
@@ -4,21 +4,22 @@ import os
 
 from tqdm import tqdm 
 import numpy as np
+from scipy.sparse.csgraph import laplacian as scipy_laplacian
 
-from utils_vis import visualize_diffusion_matrix
+from utils_vis import visualize_diffusion_timeplot
 
 class DiffusionSimulation:
     def __init__(self, connect_matrix):
-        self.beta = 1.5  #As in the Raj et al. papers
-        self.iterations = int(1e5) #1000
+        self.beta = 1.5 #As in the Raj et al. papers
+        self.iterations = int(1e3) #1000
         self.rois = 116 #AAL atlas has 116 rois
         self.tstar = 10.0
         self.timestep = self.tstar / self.iterations
         self.cm = connect_matrix
         
-    def run(self, downsample=True):
+    def run(self, inverse_log=True, downsample=True):
         ''' Run simulation. '''
-        self.calc_exponent()
+        if inverse_log: self.calc_exponent()
         self.define_seeds()
         self.calc_laplacian()
         self.diffusion_final = self.iterate_spreading()
@@ -33,22 +34,45 @@ class DiffusionSimulation:
         self.diffusion_init[[31, 32, 35, 36]] = 1
         
     def calc_laplacian(self):
+        # normed laplacian 
         adjacency = self.cm
-        degrees = np.sum(adjacency, axis=1)
+        degrees = np.sum(adjacency, axis=1) # total no. of. connections to other vertices
         dm12 = np.diag(1.0 / np.sqrt(degrees))
-        laplacian = np.eye(adjacency.shape[0]) - (dm12 @ adjacency) @ dm12
-        self.eigvals, self.eigvecs = np.linalg.eig(laplacian)
+        laplacian1 = np.eye(adjacency.shape[0]) - (dm12 @ adjacency) @ dm12 
+        
+        adjacency_matrix = self.cm
+        degree_matrix = np.diag(np.sum(adjacency_matrix, axis=1)) # total no. of. connections to other vertices
+        laplacian2= degree_matrix - adjacency_matrix
+        
+        laplacian3 = scipy_laplacian(adjacency, normed=True)
+        
+        print(np.allclose(laplacian1, laplacian2))
+        print(np.allclose(laplacian1, laplacian3))
+        print(np.allclose(laplacian2, laplacian3))
+
+        self.eigvals, self.eigvecs = np.linalg.eig(laplacian3)
     
     def integration_step(self, x0, t):
         xt = self.eigvecs.T @ x0
         xt = np.diag(np.exp(-self.beta * t * self.eigvals)) @ xt
-        return self.eigvecs @ xt    
+        return self.eigvecs @ xt  
+    
+    def integration_step_vol2(self, x0, t):
+        xt = self.eigvecs.T @ x0
+        
+        # print(np.allclose(np.linalg.inv(self.eigvecs), self.eigvecs.T))
+        
+        aux_var = (np.ones(len(self.eigvals[1:])) - np.exp(-self.beta * t * self.eigvals[1:])) / (self.beta * self.eigvals[1:])
+        d = np.insert(aux_var, 0, t)
+        # print(xt.shape, aux_var.shape, len(d), self.eigvecs.shape)
+        d  = np.diag(d) @ xt
+        return self.eigvecs @ d 
     
     def iterate_spreading(self):  
         diffusion = [self.diffusion_init]  #List containing all timepoints
 
         for _ in tqdm(range(self.iterations)):
-            next_step = self.integration_step(diffusion[-1], self.timestep)
+            next_step = self.integration_step_vol2(diffusion[-1], self.timestep)
             diffusion.append(next_step)  
             
         return np.asarray(diffusion)   
@@ -81,7 +105,7 @@ def main():
     simulation.run()
     simulation.save_matrix(output_path)
     
-    visualize_diffusion_matrix(simulation.diffusion_final)
+    visualize_diffusion_timeplot(simulation.diffusion_final)
     
 if __name__ == '__main__':
     main()
