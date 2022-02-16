bf01b02d9b1711d0a892458da2674c48b314aae7
Luca Gherardini <luca.gherardini96@gmail.com>
2022-02-16 16:00:07 +0100

    fix name of output file for ESM

--- src/simulations/simulation_ESM.py
+++ src/simulations/simulation_ESM.py
@@ -396,7 +396,7 @@ if __name__=="__main__":
         total_pcc.append(np.mean(test_pcc_list, axis=0))
    
     pt_avg.add_row([format(np.mean(total_rmse, axis=0), '.2f'), format(np.std(total_rmse, axis=0), '.2f'), format(np.mean(total_pcc, axis=0), '.2f'), format(np.std(total_pcc, axis=0), '.2f')])
-    filename = f"../../results/{datetime.now().strftime('%y-%m-%d_%H:%M:%S')}_EMS_{category}.txt"
+    filename = f"../../results/{datetime.now().strftime('%y-%m-%d_%H:%M:%S')}_ESM_{category}.txt"
     out_file = open(filename, 'w')
     out_file.write(f"Category: {category}\n")
     out_file.write(f"Cores: {num_cores}\n")
