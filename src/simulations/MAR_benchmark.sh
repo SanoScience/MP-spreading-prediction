729a2d27c77ee5c7d4b9504497ff745d372e53cb
Luca Gherardini <luca.gherardini96@gmail.com>
2022-02-23 09:54:55 +0100

    fix training set size for MAR benchmark

--- src/simulations/MAR_benchmark.sh
+++ src/simulations/MAR_benchmark.sh
@@ -9,36 +9,36 @@ echo "#"
 
 echo "### AD ###"
 
-python3 simulation_MAR.py AD 8 30 0.5 2000000 2
+python3 simulation_MAR.py AD 8 2 0.5 2000000 2
 echo "#"
-python3 simulation_MAR.py AD 8 30 0 2000000 2
+python3 simulation_MAR.py AD 8 2 0 2000000 2
 echo "#"
-python3 simulation_MAR.py AD 8 30 1 2000000 2
+python3 simulation_MAR.py AD 8 2 1 2000000 2
 echo "#"
 
 echo "### LMCI ###"
 
-python3 simulation_MAR.py LMCI 8 30 0.5 2000000 2
+python3 simulation_MAR.py LMCI 8 13 0.5 2000000 2
 echo "#"
-python3 simulation_MAR.py LMCI 8 30 0 2000000 2
+python3 simulation_MAR.py LMCI 8 13 0 2000000 2
 echo "#"
-python3 simulation_MAR.py LMCI 8 30 1 2000000 2
+python3 simulation_MAR.py LMCI 8 13 1 2000000 2
 echo "#"
 
 echo "### EMCI ###"
 
-python3 simulation_MAR.py EMCI 8 30 0.5 2000000 2
+python3 simulation_MAR.py EMCI 8 14 0.5 2000000 2
 echo "#"
-python3 simulation_MAR.py EMCI 8 30 0 2000000 2
+python3 simulation_MAR.py EMCI 8 14 0 2000000 2
 echo "#"
-python3 simulation_MAR.py EMCI 8 30 1 2000000 2
+python3 simulation_MAR.py EMCI 8 14 1 2000000 2
 echo "#"
 
 echo "### CN ###"
 
-python3 simulation_MAR.py CN 8 30 0.5 2000000 2
+python3 simulation_MAR.py CN 8 8 0.5 2000000 2
 echo "#"
-python3 simulation_MAR.py CN 8 30 0 2000000 2
+python3 simulation_MAR.py CN 8 8 0 2000000 2
 echo "#"
-python3 simulation_MAR.py CN 8 30 1 2000000 2
+python3 simulation_MAR.py CN 8 8 1 2000000 2
 echo "#"
