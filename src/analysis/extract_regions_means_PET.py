fix and improve simulations and data normalization

src/analysis/extract_regions_means_PET.py
@@ -6,13 +6,13 @@ import os
 from glob import glob
 import logging
 import csv
-from statistics import mean
+import time
 from tqdm import tqdm
 from nibabel import Nifti1Image, save, load
 import numpy as np
 from skimage.util import montage
 import matplotlib.pyplot as plt
-from threading import Thread, active_count, Lock
+from threading import Thread, Lock
 from multiprocessing import cpu_count
 import yaml
 from datetime import datetime
@@ -48,10 +48,35 @@ class pet_loader(Thread):
 
     def load_pet(self):
         pet_data = load(self.pet).get_fdata()
-        pet_data /= np.max(pet_data)
+        if np.sum(pet_data) == 0:
+            print(f"File {self.pet} is empty!")
+            logging.error(f"File {self.pet} is empty!")
+        #pet_data /= np.max(pet_data)
         return pet_data
 
-    def extract_avg_region_means(self, pet_data):
+    # NOTE: this function override the homonyms 'run' in the Thread class, executed when the method 'start' is invoked
+    def run(self):  
+        pet_data = self.load_pet()
+        
+        id = self.pet.split('/')[-1].split('.')[0].split('_')[0]
+        if 'ses-baseline' in self.pet:
+            session = 'baseline'
+        else:
+            session = 'followup'
+            
+        lock.acquire()        
+        subj_pet[id].append([session, pet_data])
+        lock.release()
+
+class pet_average(Thread):
+    def __init__(self, file_id, pet_id, pet_data, max_val):
+        Thread.__init__(self)
+        self.file_id = file_id
+        self.pet_id = pet_id
+        self.pet_data = pet_data
+        self.max_val = max_val
+        
+    def extract_avg_region_means(self):
         means = []
         # do not take atlas region with index=0, which indicates the background
         # there are 166 unique labels (35, 36, 81, 82 are missing)
@@ -61,48 +86,44 @@ class pet_loader(Thread):
             try:
                 # NOTE: storing indices doesn't work! (see below)
                 # indices = np.where(atlas == label)
-                avg = pet_data[np.where(atlas_data == label)].mean()
-                pet_data[np.where(atlas_data == label)] = avg
+                avg = self.pet_data[np.where(atlas_data == label)].mean()
+                self.pet_data[np.where(atlas_data == label)] = avg
             except Exception as e:
-                logging.error(f"Invalid index for image {pet}")
-                print(f"Invalid index for image {pet}")
+                logging.error(f"Invalid index for image {self.pet_id}")
+                print(f"Invalid index for image {self.pet_id}")
                 avg = 0
             means.append(avg)
         
         # put what is not in the atlas to 0 (i.e. skull)
         try:
-            pet_data[np.where(atlas_data==0)] = 0
-            np.nan_to_num(pet_data, copy=False, nan=0, posinf=0, neginf=0)
+            self.pet_data[np.where(atlas_data==0)] = 0
+            np.nan_to_num(self.pet_data, copy=False, nan=0, posinf=0, neginf=0)
         except Exception as e:
             logging.error(f"Error during background remotion. Traceback: {e}")
             print(f"Error during background remotion. Traceback: {e}")
         
         # return mean concentration for each region and the new voxels with value corresponding to correspondent regional concentration
-        return np.array(means)
+        return np.array(np.mean(means, axis=0))
 
-    # NOTE: this function override the homonyms 'run' in the Thread class, executed when the method 'start' is invoked
-    def run(self):  
-        pet_data = self.load_pet()
-        means = self.extract_avg_region_means(pet_data)
-        
+    def run(self):
+        self.pet_data /= self.max_val
+        means = self.extract_avg_region_means()
         lock.acquire()
-        concentrations[self.pet] = means
-        voxels[self.pet] = pet_data
-        if 'sub-AD' in self.pet:
-            ad_voxels.append(pet_data)
-        elif 'sub-LMCI' in self.pet:
-            lmci_voxels.append(pet_data)
-        elif 'sub-MCI' in self.pet:
-            mci_voxels.append(pet_data)
-        elif 'sub-EMCI' in self.pet:
-            emci_voxels.append(pet_data)
-        if 'sub-CN' in self.pet:      
-            cn_voxels.append(pet_data)
-            if 'ses-baseline' in self.pet:
-                cn_baseline_region.append(concentrations[self.pet])
-                cn_baseline_voxels.append(pet_data)
+        voxels[self.file_id] = means
+        if 'sub-AD' in self.pet_id:
+            ad_voxels.append(means)
+        elif 'sub-LMCI' in self.pet_id:
+            lmci_voxels.append(means)
+        elif 'sub-MCI' in self.pet_id:
+            mci_voxels.append(means)
+        elif 'sub-EMCI' in self.pet_id:
+            emci_voxels.append(means)
+        if 'sub-CN' in self.pet_id:      
+            cn_voxels.append(means)
+            if 'ses-baseline' in self.pet_id:
+                cn_baseline_region.append(means)
+                cn_baseline_voxels.append(means)
         lock.release()
-
 
 if __name__ == '__main__':
     ## INPUT
@@ -131,6 +152,7 @@ if __name__ == '__main__':
     pets = glob(dataset_dir)
     
     # NOTE: the following variables will be visible inside the Thread without needing for importing! They will always point to the same structure    
+    subj_pet = defaultdict(list)
     concentrations = {}
     voxels = {}
     
@@ -142,42 +164,57 @@ if __name__ == '__main__':
     cn_baseline_voxels = []
     cn_baseline_region = []
     lock = Lock()     
+    works = []
 
-    ### PET PROCESSING
+    ### PET READING
     print('Reading PETs...')
     for i in tqdm(range(len(pets))):
-        pet_loader(pets[i]).start()
+        works.append(pet_loader(pets[i]))
+        works[-1].start()
 
-        # The currect thread counts in the active_count()
-        while active_count() > num_cores+1:
-            pass
+        while len(works) >= num_cores:
+            for w in works:
+                w.join(timeout=0.1)
+                if not w.is_alive():
+                    works.remove(w)
     
-    # NOTE: after starting the threading, the number of active threads will never go below 2 (Main and Monitor threads)
-    while active_count() > 2:
-        pass
-
-    # NOTE: EXPERIMENTAL
-    for s in tqdm(concentrations.keys()):
-        try:
-            # TODO: Modify this and test
-            # z-score (region(i) - healthy_mean(i))/healthy_std(i)
-            
-            # standard logistic function e^(region[i]) / ( e^(region(i)) + 1 )
-            #concentrations[s] = 1 / (1 + np.exp(- concentrations[s]))  
-            
-            output_path = s.replace('.nii.gz', '.csv')          
-            save_concentrations(concentrations[s], output_path)
-        except Exception as e:
-            logging.error(f"Error in normalization of subject {s} regional concentrations. Traceback: {e}")
-            print(f"Error in normalization of subject {s} regional concentrations. Traceback: {e}")
-
-    quit()
+    for w in works:
+        w.join()
+        works.remove(w)
         
+    
+    ### NORMALIZE VALUES USING THE MAXIMUM VALUE BETWEEN BASELINE AND FOLLOWUP
+    print('Normalizing PETs...')
+    for subj_id in tqdm(subj_pet):
+        max_val = 0
+        for data in subj_pet[subj_id]:
+            tmp = np.max(data[1])
+            if max_val < tmp:
+                max_val = tmp
+                
+        for data in subj_pet[subj_id]:
+            file_id = ''
+            for p in pets:
+                if data[0] in p and subj_id in p:
+                    file_id = p
+                    break
+            works.append(pet_average(file_id, subj_id, data[1], max_val))
+            works[-1].start()
+        while len(works) >= num_cores:
+            for w in works:
+                if not w.is_alive():
+                    works.remove(w)
+    
+    for w in works:
+        w.join()
+        works.remove(w)   
+    
+    time.sleep(3)
+    
     ### AVERAGE PET VOXELS
     print('Averaging PETs voxels...')
     try:
         ad_voxels = np.array(ad_voxels)
-        np.nan_to_num(ad_voxels, copy=False, nan=0)
         ad_voxels = np.mean(ad_voxels, axis=0, dtype=np.float64)
         ad_voxels[np.where(atlas_data==0)] = 0
     except Exception as e:
@@ -185,7 +222,6 @@ if __name__ == '__main__':
         print(f"Exception during averaging of AD PETs voxels. Traceback: {e}")
     try:
         lmci_voxels = np.array(lmci_voxels)
-        np.nan_to_num(lmci_voxels, copy=False, nan=0)
         lmci_voxels = np.mean(lmci_voxels, axis=0, dtype=np.float64)
         lmci_voxels[np.where(atlas_data==0)] = 0
     except Exception as e:
@@ -193,7 +229,6 @@ if __name__ == '__main__':
         print(f"Exception during averaging of LMCI PETs voxels. Traceback: {e}")
     try:
         mci_voxels = np.array(mci_voxels)
-        np.nan_to_num(mci_voxels, copy=False, nan=0)
         mci_voxels = np.mean(mci_voxels, axis=0, dtype=np.float64)
         mci_voxels[np.where(atlas_data==0)] = 0
     except Exception as e:
@@ -201,7 +236,6 @@ if __name__ == '__main__':
         print(f"Exception during averaging of MCI PETs voxels. Traceback: {e}")
     try:
         emci_voxels = np.array(emci_voxels)
-        np.nan_to_num(emci_voxels, copy=False, nan=0)
         emci_voxels = np.mean(emci_voxels, axis=0, dtype=np.float64)
         emci_voxels[np.where(atlas_data==0)] = 0
     except Exception as e:
@@ -209,7 +243,6 @@ if __name__ == '__main__':
         print(f"Exception during averaging of EMCI PETs voxels. Traceback: {e}")
     try:
         cn_voxels = np.array(cn_voxels)
-        np.nan_to_num(cn_voxels, copy=False, nan=0)
         cn_voxels = np.mean(cn_voxels, axis=0, dtype=np.float64)
         cn_voxels[np.where(atlas_data==0)] = 0
     except Exception as e:
@@ -253,12 +286,10 @@ if __name__ == '__main__':
     try:
         # REGIONS
         cn_baseline_region = np.array(cn_baseline_region)
-        np.nan_to_num(cn_baseline_region, copy=False, nan=0, posinf=0, neginf=0)
         
         cn_baseline_mean = np.mean(cn_baseline_region, axis=0, dtype=np.float64)
         
         cn_baseline_std = np.std(cn_baseline_region, axis=0, dtype=np.float64)
-        np.nan_to_num(cn_baseline_std, copy=False, nan=1, posinf=1, neginf=1)
         cn_baseline_std[cn_baseline_std == 0] = 1
         
         np.savetxt('cn_baseline_mean.csv', cn_baseline_mean, delimiter=', ')
@@ -266,13 +297,11 @@ if __name__ == '__main__':
 
         # VOXELS
         cn_baseline_voxels = np.array(cn_baseline_voxels)
-        np.nan_to_num(cn_baseline_voxels, copy=False, nan=0, posinf=0, neginf=0)
         
         cn_voxels_mean = np.mean(cn_baseline_voxels, axis = 0, dtype=np.float64)
         cn_voxels_mean[np.where(atlas_data==0)] = 0
         
         cn_voxels_std = np.std(cn_baseline_voxels, axis = 0, dtype=np.float64)
-        np.nan_to_num(cn_voxels_std, copy=False, nan=1, posinf=1, neginf=1)
         cn_voxels_std[cn_voxels_std == 0] = 1
         cn_voxels_std[np.where(atlas_data==0)] = 1 
     except Exception as e:
@@ -290,7 +319,7 @@ if __name__ == '__main__':
         try:
             # TODO: Modify this and test
             # z-score (region(i) - healthy_mean(i))/healthy_std(i)
-            concentrations[s] = (concentrations[s] - cn_baseline_mean) / cn_baseline_std
+            #concentrations[s] = (concentrations[s] - cn_baseline_mean) / cn_baseline_std
             
             # standard logistic function e^(region[i]) / ( e^(region(i)) + 1 )
             #concentrations[s] = 1 / (1 + np.exp(- concentrations[s]))  
@@ -375,15 +404,16 @@ if __name__ == '__main__':
         logging.error(f"Error in saving CN normalized concentrations. Traceback: {e}")
         print(f"Error in saving CN normalized concentrations. Traceback: {e}")
     
+    '''
     logging.info("Z-score normalization of subjects regional concentrations")
     print("Z-score normalization of subjects regional concentrations")
     for pet in tqdm(voxels.keys()):
         try:
             # z-score (region(i) - healthy_mean(i))/healthy_std(i)    
-            voxels[pet] = (voxels[pet] - cn_voxels_mean)/cn_voxels_std
+            #voxels[pet] = (voxels[pet] - cn_voxels_mean)/cn_voxels_std
             
             # logistic transformation
-            voxels[pet] = 1 / (1 + np.exp(-voxels[pet])) 
+            #voxels[pet] = 1 / (1 + np.exp(-voxels[pet])) 
             voxels[pet][np.where(atlas_data==0)] = 0
             
             output_path = pet.replace('.nii.gz', '_avg.nii.gz')          
@@ -391,5 +421,5 @@ if __name__ == '__main__':
         except Exception as e:
             logging.error(f"Error in z-score normalization of subjects {pet} regional concentration: {e}")
             print(f"Error in z-score normalization of subjects {pet} regional concentration: {e}")
-       
+    '''
     logging.info('*** Done ***')

src/simulations/simulation_ESM.py
@@ -14,12 +14,11 @@ Authors: Yasser Iturria-Medina ,Roberto C. Sotero,Paule J. Toussaint,Alan C. Eva
 
 from datetime import datetime
 import json
-from threading import Thread, active_count, Lock
+from threading import Thread, Lock
 from multiprocessing import cpu_count
 import os
 import logging
 import sys
-from tempfile import tempdir
 from time import time
 import warnings
 from prettytable import PrettyTable
@@ -35,6 +34,7 @@ from utils import drop_data_in_connect_matrix, load_matrix
 np.seterr(all = 'raise')
 date = datetime.now().strftime('%y-%m-%d_%H:%M:%S')
 logging.basicConfig(format='%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s', datefmt='%Y-%m-%d,%H:%M:%S', level=logging.INFO, force=True, filename = f"trace_ESM_{date}.log")
+digits = 5
 
 class ESM(Thread):       
     
@@ -160,7 +160,7 @@ class ESM(Thread):
         total_mse.append(mse)
         total_pcc.append(pcc)
         total_reg_err.append(reg_err)
-        pt_subs.add_row([subj, round(mse,4), round(pcc,4)])
+        pt_subs.add_row([subj, round(mse,digits), round(pcc,digits)])
         lock.release()
             
         return
@@ -248,18 +248,24 @@ if __name__=="__main__":
     total_time = time()
 
     lock = Lock()
+    
+    works = []
     for subj, paths in tqdm(dataset.items()):
-        ESM(paths, subj).start()
-        while active_count() > num_cores + 1:
-            pass
+        works.append(ESM(paths, subj))
+        works[-1].start()
+        while len (works) >= num_cores:
+            for w in works:
+                if not w.is_alive():
+                    works.remove(w)
             
-    while active_count() > 2:
-        pass  
+    for w in works:
+        w.join()
+        works.remove(w)
         
     ### OUTPUT ###
    
     np.savetxt(f"{output_mat}ESM_{category}_regions_{date}.csv", np.mean(np.array(total_reg_err), axis=0), delimiter=',')
-    pt_avg.add_row([round(np.mean(total_mse, axis=0), 4), round(np.std(total_mse, axis=0), 2), round(np.mean(total_pcc, axis=0), 4), round(np.std(total_pcc, axis=0), 2)])
+    pt_avg.add_row([round(np.mean(total_mse, axis=0), digits), round(np.std(total_mse, axis=0), 2), round(np.mean(total_pcc, axis=0), digits), round(np.std(total_pcc, axis=0), 2)])
 
     total_time = time() - total_time
     filename = f"{output_res}ESM_{datetime.now().strftime('%y-%m-%d_%H:%M:%S')}.txt"

src/simulations/simulation_MAR.py
@@ -29,7 +29,7 @@ from sklearn.metrics import mean_squared_error
 np.seterr(all = 'raise')
 date = str(datetime.now().strftime('%y-%m-%d_%H:%M:%S'))
 logging.basicConfig(format='%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s', datefmt='%Y-%m-%d,%H:%M:%S', level=logging.INFO, force=True, filename = f"trace_MAR_{date}.log")
-
+digits = 5
 
 class MAR(Thread):
     def __init__(self, subject, paths):
@@ -118,7 +118,7 @@ class MAR(Thread):
                 
                 iter_count += 1
                 
-            if error_reconstruct <= self.error_stop or bad_iter_count == self.max_bad_iter or iter_count == self.iter_max or norm <= self.gradient_thr: break
+            if error_reconstruct <= self.error_stop or bad_iter_count == self.max_bad_iter or iter_count == iter_max or norm <= self.gradient_thr: break
             trial += 1
             self.eta /= 10 
         
@@ -155,19 +155,21 @@ class MAR(Thread):
             pcc = pearson_corr_coef(self.t1_concentration, t1_concentration_pred)[0]
             if np.isnan(mse) or np.isinf(mse): raise Exception("Invalid value of MSE")
             if np.isnan(pcc): raise Exception("Invalid value of PCC")
+            mse = round(mse, digits)
+            pcc = round(pcc, digits)
         except Exception as e:
             logging.error(f"Exception happened for Pearson correlation coefficient and MSE computation for subject {self.subject}. Traceback:\n{e}") 
             return
-    
+
+        lock.acquire()
+        # NOTE: plots should be saved one by one without multiple threads!
         try:
             logging.info(f"Saving prediction plot for subject {self.subject}")
             save_prediction_plot(self.t0_concentration, t1_concentration_pred, self.t1_concentration, self.subject, self.subject + 'train/MAR_train_' + date + '.png', mse, pcc)
             save_coeff_matrix(self.subject + 'train/MAR_train_' + date + '.csv', self.A)
         except Exception as e:
             logging.error(f"Exception happened during saving of simulation results for subject {self.subject}. Traceback:\n{e}")
-            return
-
-        lock.acquire()
+            
         training_scores[self.subject] = [mse, pcc]
         lock.release()
 
@@ -175,14 +177,18 @@ class MAR(Thread):
 
 def training():
     
+    works = []
     for subj, paths in train_set.items():
-        MAR(subj, paths).start()      
+        works.append(MAR(subj, paths))
+        works[-1].start()      
 
-        while active_count() > num_cores + 1:
-            pass
+        while len(works) >= num_cores:
+            for w in works:
+                if not w.is_alive(): 
+                    works.remove(w)
     
-    while active_count() > 2:
-        pass
+    for w in works:
+        w.join()
     
     coeff_matrices = []
     # read results saved by "run_simulation method"
@@ -363,7 +369,7 @@ if __name__ == '__main__':
 
     pt_avg = PrettyTable()
     pt_avg.field_names = ["Avg MSE test", "SD MSE test", "Avg Pearson test", "SD Pearson test"]
-    pt_avg.add_row([round(np.mean(total_mse), 5), round(np.std(total_mse), 2), round(np.mean(total_pcc), 5), round(np.std(total_pcc), 2)])
+    pt_avg.add_row([round(np.mean(total_mse), digits), round(np.std(total_mse), 2), round(np.mean(total_pcc), digits), round(np.std(total_pcc), 2)])
         
     pt_train = PrettyTable()
     pt_train.field_names = ["ID", "Avg MSE train", "Avg Pearson train"]
@@ -372,8 +378,7 @@ if __name__ == '__main__':
     for s in training_scores.keys():
         mse_subj = [training_scores[s][0]]
         pcc_subj = [training_scores[s][1]]
-        pt_train.add_row([s, round(np.mean(mse_subj), 5), round(np.std(mse_subj), 2), round(np.mean(pcc_subj), 5), round(np.std(pcc_subj), 2)])
-        pt_train.add_row([s, round(np.mean(mse_subj), 5), round(np.mean(pcc_subj), 5)])
+        pt_train.add_row([s, round(np.mean(mse_subj), digits), round(np.mean(pcc_subj), digits)])
 
     pt_test = PrettyTable()
     pt_test.field_names = ["ID", "Avg MSE test", "Avg Pearson test"]
@@ -382,7 +387,7 @@ if __name__ == '__main__':
     for s in test_scores.keys():
         mse_subj = [test_scores[s][0]]
         pcc_subj = [test_scores[s][1]]
-        pt_test.add_row([s, round(np.mean(mse_subj), 5), round(np.mean(pcc_subj), 5)])
+        pt_test.add_row([s, round(np.mean(mse_subj), digits), round(np.mean(pcc_subj), digits)])
 
     total_time = time() - total_time
     filename = f"{output_res}MAR_{category}_{date}.txt"

src/simulations/simulation_NDM.py
@@ -33,7 +33,7 @@ import warnings
 np.seterr(all = 'raise')
 date = datetime.now().strftime('%y-%m-%d_%H:%M:%S')
 logging.basicConfig(format='%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s', datefmt='%Y-%m-%d,%H:%M:%S', level=logging.ERROR, force=True, filename = f"trace_NDM_{date}.log")
-
+digits = 5
 
 class NDM(Thread):
     def __init__(self, subject, paths, downsample = False):
@@ -175,7 +175,7 @@ class NDM(Thread):
         mse_list.append(mse)
         pcc_list.append(pcc)
         reg_err_list.append(reg_err)
-        pt_subs.add_row([self.subject, round(mse,4), round(pcc,4)])
+        pt_subs.add_row([self.subject, round(mse,digits), round(pcc,digits)])
         return
     
 if __name__ == '__main__':
@@ -236,20 +236,26 @@ if __name__ == '__main__':
     
     total_time = time()
     
+    works = []
     for subj, paths in dataset.items():
-        NDM(subj, paths).start()
+        works.append(NDM(subj, paths))
+        works[-1].start()
 
-        while active_count() > num_cores + 1:
-            pass 
+        while len(works) >= num_cores:
+            for w in works:
+                if not w.is_alive():
+                    works.remove(w)
+                    break
 
-    while active_count() > 2:
-        pass
+    for w in works:
+        w.join()
+        works.remove(w)
         
     
     ### OUTPUTS ###
 
     np.savetxt(f"{output_mat}NDM_{category}_regions_{date}.csv", np.mean(np.array(reg_err_list), axis=0), delimiter=',')
-    pt_avg.add_row([round(np.mean(mse_list, axis=0), 4), round(np.std(mse_list, axis=0), 2), round(np.mean(pcc_list, axis=0), 4), round(np.std(pcc_list, axis=0), 2)])
+    pt_avg.add_row([round(np.mean(mse_list, axis=0), digits), round(np.std(mse_list, axis=0), 2), round(np.mean(pcc_list, axis=0), digits), round(np.std(pcc_list, axis=0), 2)])
 
     total_time = time() - total_time
     filename = f"{output_res}/NDM_{category}_{datetime.now().strftime('%y-%m-%d_%H:%M:%S')}.txt"

src/simulations/utils_vis.py
@@ -3,11 +3,12 @@
 import os
 
 import matplotlib.pyplot as plt
+
 import numpy as np
 import seaborn as sns
 
-sns.set_theme(style="darkgrid")
-sns.set_palette(['#390099', '#FF0054', '#00A6FB'])
+#sns.set_theme(style="darkgrid")
+#sns.set_palette(['#390099', '#FF0054', '#00A6FB'])
 
 def visualize_NIfTI_data(data, depths, time_idx=None):
     ''' Visualize provided depths from NIfTI data at the specific time. 
@@ -44,35 +45,31 @@ def visualize_diffusion_timeplot(matrix, timestep, total_time, save_dir=None):
         plt.savefig(os.path.join(save_dir, 'diffusion_over_time.png'))
     plt.show() 
     
-def save_prediction_plot(baseline, prediction, followup, subject, filepath, error=None, corr_coeff=None):
-    '''
-    plt.figure(figsize=(15, 5))
-    plt.plot(baseline, '-', marker='o', c='b', label='initial concentration t0')
-    plt.plot(prediction, '--', marker='o', c='r', label='predicted concentration t1')
-    plt.plot(followup, '-', marker='o', c='g', label='true concentration t1')
-    plt.xlabel('ROI (index of brain region based on AAL atlas)')
-    plt.ylabel('concentration of misfolded proteins')
-    plt.legend(loc='upper right')
-    if error is not None: plt.title(f'Subject: {subject} \nError between true and predicted t1: {error:.2f}\nPearson correlation coeff: {corr_coeff:.2f}')
-    plt.tight_layout()
-    plt.savefig(filepath)
-    # After saving the figure, explicitly close it to avoid memory wasting
-    plt.close()
-    '''
+def save_prediction_plot(baseline, prediction, followup, subject, filepath, error=None, corr_coeff=None):    
     plt.figure(figsize=(20, 15))
-    sns.lineplot(data=baseline, label='baseline concentration', marker='o', dashes=False, markers=True)
-    sns.lineplot(data=prediction, label='predicted followup concentration', marker='o', dashes=True, markers=True)
-    sns.lineplot(data=followup, label='followup concentration', marker='o', dashes=False, markers=True)
+    plt.plot(baseline, '-', marker='o', c='#390099', label='baseline concentration', linewidth=2)
+    plt.plot(followup, '-', marker='o', c='#00A6FB', label='followup concentration', linewidth=2)
+    plt.plot(prediction, '-', marker='o', c='#FF0054', label='predicted concentration', linewidth=4, alpha = 0.5)
+        
+    '''
+    plt.figure(figsize=(25, 10))
+    sns.lineplot(data=baseline, label='baseline concentration', marker='o', dashes=False, markers=True, linewidth=1)
+    sns.lineplot(data=followup, label='followup concentration', marker='o', dashes=False, markers=True, linewidth=1)
+    sns.lineplot(data=prediction, label='predicted followup concentration', marker='o', dashes=True, markers=True, linewidth=3, alpha = 0.5)
+    '''
     
-    plt.legend(bbox_to_anchor=(1, 1.06), loc='upper right', borderaxespad=0.)
-    plt.xlabel('ROI (index of brain region based on AAL3 atlas)')
-    plt.ylabel('concentration of misfolded proteins')
-    
-    if error is not None: plt.title(f'Subject: {subject.split(os.sep)[-2]} \nError between true and predicted t1: {error:.5f}\nPearson correlation coeff: {corr_coeff:.5f}')
+    plt.legend(bbox_to_anchor=(1, 1.065), loc='upper right', borderaxespad=0.1, fontsize=12)
+    plt.xlabel('ROI (166 regions of AAL3 atlas)', fontsize=12)
+    plt.ylabel('concentration of misfolded proteins', fontsize=12)
+    plt.xlim(-1, len(baseline))
+    plt.xticks(np.arange(0, len(baseline), step=5), fontsize=12)
+    plt.grid(True)
     plt.tight_layout()
+    
+    if error is not None: plt.title(f'Subject: {subject.split(os.sep)[-2]} \nError between true and predicted t1: {error}\nPearson correlation coeff: {corr_coeff}')
     plt.savefig(filepath)
     plt.close()
-
+    
     return 
     
 def visualize_error(error):